1eacaf59280bfcb73056c6ab7dac7560130412c7
Digvijay Ukirde <digvijay.ukirde@gmail.com>
2023-10-19 11:37:45 -0400

    Verify pre-commit check (required_providers)

--- modules/inventory/version.tf
+++ modules/inventory/version.tf
@@ -1,3 +1,9 @@
 terraform {
   required_version = ">= 1.3, < 1.6"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2"
+    }
+  }
 }
--- modules/key/version.tf
+++ modules/key/version.tf
@@ -1,3 +1,13 @@
 terraform {
   required_version = ">= 1.3, < 1.6"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4"
+    }
+  }
 }
