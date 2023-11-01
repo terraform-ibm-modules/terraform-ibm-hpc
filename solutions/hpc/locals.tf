ab718f21db755036635c427e69c9854b8407d67f
Digvijay Ukirde <digvijay.ukirde@gmail.com>
2023-10-31 22:16:32 -0400

    Conditional skip_iam_authorization_policy setting

--- solutions/hpc/locals.tf
+++ solutions/hpc/locals.tf
@@ -1,7 +1,8 @@
 # locals needed for landing_zone
 locals {
   # Region and Zone calculations
-  region = join("-", slice(split("-", var.zones[0]), 0, 2))
+  region                         = join("-", slice(split("-", var.zones[0]), 0, 2))
+  boot_volume_encryption_enabled = var.key_management != null ? true : false
 }
 
 # locals needed for bootstrap
