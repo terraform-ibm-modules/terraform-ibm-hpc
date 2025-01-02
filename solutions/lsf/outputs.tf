8ecec2e769d14387941215aa9693eae31803e527
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-01-02 12:22:00 +0530

    modified output
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- solutions/lsf/outputs.tf
+++ solutions/lsf/outputs.tf
@@ -1,4 +1,9 @@
-output "lsf" {
-  value = module.lsf.file_storage
+# output "lsf" {
+#   value = module.lsf.file_storage
+#   #sensitive = true
+# }
+
+output "ssh_to_compute" {
+  value = module.lsf.ssh_to_compute
   #sensitive = true
 }
