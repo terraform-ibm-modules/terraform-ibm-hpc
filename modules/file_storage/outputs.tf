eb794a1f2bfd4bb5e70888dbafa893643511a2fe
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-02-10 15:14:26 +0530

    removed Duplicate Variable
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- modules/file_storage/outputs.tf
+++ modules/file_storage/outputs.tf
@@ -10,8 +10,3 @@ output "name_mount_path_map" {
   description = "Mount path name and its path map"
   value = { for mount_details in flatten([ibm_is_share_mount_target.share_target_vpc, ibm_is_share_mount_target.share_target_sg]) : split("-", mount_details.name)[length(split("-", mount_details.name)) - 4] => mount_details.mount_path }
 }
-
-output "name_mount_path_map" {
-  description = "Mount path name and its path map"
-  value = { for mount_details in flatten([ibm_is_share_mount_target.share_target_vpc, ibm_is_share_mount_target.share_target_sg]) : split("-", mount_details.name)[length(split("-", mount_details.name)) - 4] => mount_details.mount_path }
-}
