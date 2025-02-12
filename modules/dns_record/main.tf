95e5808bbca1f0fad46771df547d29447da2b037
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-02-12 16:17:01 +0530

    added con
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- modules/dns_record/main.tf
+++ modules/dns_record/main.tf
@@ -24,7 +24,7 @@ resource "ibm_dns_resource_record" "ptr" {
   zone_id     = var.dns_zone_id
   type        = "PTR"
   name        = var.dns_records[count.index]["rdata"]
-  rdata       = format("%s.%s", var.dns_records[count.index]["name"], one(local.dns_domain_name))
+  rdata       = format("%s.%s", var.dns_records[count.index]["name"], "comp.com")
   ttl         = 300
   depends_on  = [ibm_dns_resource_record.a]
 }
