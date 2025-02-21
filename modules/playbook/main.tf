01276ab70cb58e67ec3874902a83c18e72b04713
Anand.Reddy7 <Anand.Reddy7@ibm.com>
2025-02-21 17:11:05 +0530

    Adding Hyperthreading configuration

--- modules/playbook/main.tf
+++ modules/playbook/main.tf
@@ -83,3 +83,64 @@ resource "null_resource" "run_playbook" {
 
 #   depends_on = [null_resource.run_playbook]
 # }
+
+resource "local_file" "create_playbook_for_management" {
+  count    = var.inventory_path != null && var.enable_lsf ? 1 : 0
+  content  = <<EOT
+# Ensure provisioned VMs are up and Passwordless SSH setup has been established
+
+- name: Check passwordless SSH connection is setup
+  hosts: [all_nodes]
+  any_errors_fatal: true
+  gather_facts: false
+  vars:
+    ansible_ssh_common_args: >
+      ${local.proxyjump}
+      -o ControlMaster=auto
+      -o ControlPersist=30m
+      -o UserKnownHostsFile=/dev/null
+      -o StrictHostKeyChecking=no
+    ansible_user: root
+    ansible_ssh_private_key_file: ${var.private_key_path}
+  tasks:
+    - name: Check passwordless SSH on all scale inventory hosts
+      shell: echo PASSWDLESS_SSH_ENABLED
+      register: result
+      until: result.stdout.find("PASSWDLESS_SSH_ENABLED") != -1
+      retries: 60
+      delay: 10
+
+- name: Prerequisite Configuration
+  hosts: "{{ groups['all_nodes'][0] }}"
+  any_errors_fatal: true
+  gather_facts: True
+  vars:
+    ansible_ssh_common_args: >
+      ${local.proxyjump}
+      -o ControlMaster=auto
+      -o ControlPersist=30m
+      -o UserKnownHostsFile=/dev/null
+      -o StrictHostKeyChecking=no
+    ansible_user: root
+    ansible_ssh_private_key_file: ${var.private_key_path}
+  pre_tasks:
+    - name: Load cluster-specific variables
+      include_vars: all.json
+  roles:
+     - lsf_server_config
+    #  - lsf_mgmt_config
+EOT
+  filename = "/opt/ibm/terraform-ibm-hpc/modules/ansible-roles/management_config.yml"
+}
+
+resource "null_resource" "run_playbook_management" {
+  count = var.inventory_path != null && var.enable_lsf ? 1 : 0
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = "ansible-playbook -i ${var.inventory_path} '/opt/ibm/terraform-ibm-hpc/modules/ansible-roles/management_config.yml'"
+  }
+  triggers = {
+    build = timestamp()
+  }
+  depends_on = [local_file.create_playbook]
+}
