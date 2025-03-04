79960c1c64675b5af4d151c9edd552610d51017e
Anand.Reddy7 <Anand.Reddy7@ibm.com>
2025-03-04 14:10:45 +0530

    LSF network configurations
    Signed-off-by: Anand.Reddy7 <Anand.Reddy7@ibm.com>

--- locals.tf
+++ locals.tf
@@ -257,7 +257,6 @@ locals {
   nfs_install_dir       = "none"
   Enable_Monitoring     = false
   lsf_deployer_hostname = var.deployer_hostname #data.external.get_hostname.result.name  #var.enable_bastion ? "" : flatten(module.deployer.deployer_vsi_data[*].list)[0].name
-  com_dns_domain_name   = var.enable_deployer ? "" : var.dns_domain_names["compute"]
 }
 
 locals {
