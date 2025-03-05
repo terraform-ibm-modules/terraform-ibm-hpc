c38a44cb6172e280b01fbfb3f28985bfd73fbba8
Nupur Goyal <nupurgoyal@Nupurs-MacBook-Pro.local>
2025-03-05 19:57:37 +0530

    Updating the changes from private repo

--- examples/create_vpc/solutions/hpc/main.tf
+++ examples/create_vpc/solutions/hpc/main.tf
@@ -1,13 +1,13 @@
 module "create_vpc" {
-  source               = "../../modules/landing_zone_vpc"
-  allowed_cidr         = var.remote_allowed_ips
-  ibmcloud_api_key     = var.ibmcloud_api_key
-  ssh_keys             = var.bastion_ssh_keys
-  prefix               = var.cluster_prefix
-  resource_group       = var.resource_group
-  zones                = var.zones
-  network_cidr         = var.vpc_cidr
-  bastion_subnets_cidr = var.vpc_cluster_login_private_subnets_cidr_blocks
-  compute_subnets_cidr = var.vpc_cluster_private_subnets_cidr_blocks
-  enable_hub           = var.enable_hub
+  source                  = "../../modules/landing_zone_vpc"
+  allowed_cidr            = var.remote_allowed_ips
+  ibmcloud_api_key        = var.ibmcloud_api_key
+  ssh_keys                = var.bastion_ssh_keys
+  prefix                  = var.cluster_prefix
+  existing_resource_group = var.existing_resource_group
+  zones                   = var.zones
+  network_cidr            = var.vpc_cidr
+  bastion_subnets_cidr    = var.vpc_cluster_login_private_subnets_cidr_blocks
+  compute_subnets_cidr    = var.vpc_cluster_private_subnets_cidr_blocks
+  enable_hub              = var.enable_hub
 }
