d3ef2c81aa3df53672a29d94b85a0e1d26549278
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-03-27 21:57:33 +0530

    fix26
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- modules/deployer/template_files.tf
+++ modules/deployer/template_files.tf
@@ -11,8 +11,8 @@ data "template_file" "deployer_user_data" {
     bastion_public_key_content  = local.enable_bastion ? module.ssh_key[0].public_key_content : ""
     compute_dns_domain          = local.enable_bastion ? local.compute_dns_domain : ""
     compute_interfaces          = local.enable_bastion ? local.compute_interfaces : ""
-    compute_public_key_content  = local.enable_bastion ? module.compute_key[0].public_key_content : ""
-    compute_private_key_content = local.enable_bastion ? module.compute_key[0].private_key_content : ""
+    compute_public_key_content  = local.enable_bastion && local.enable_compute ? module.compute_key[0].public_key_content : ""
+    compute_private_key_content = local.enable_bastion && local.enable_compute ? module.compute_key[0].private_key_content : ""
 
   }
 }
