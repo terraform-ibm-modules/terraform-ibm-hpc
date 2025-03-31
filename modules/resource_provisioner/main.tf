fe73cfe3991920f9664a629745731023edf1cfdf
Anand.Reddy7 <Anand.Reddy7@ibm.com>
2025-03-31 23:16:50 +0530

    Fixing Pre-commit issues
    Signed-off-by: Anand.Reddy7 <Anand.Reddy7@ibm.com>

--- modules/resource_provisioner/main.tf
+++ modules/resource_provisioner/main.tf
@@ -20,8 +20,10 @@ resource "null_resource" "tf_resource_provisioner" {
     inline = [
       "if [ ! -d ${local.remote_terraform_path} ]; then sudo git clone -b ${local.da_hpc_repo_tag} ${local.da_hpc_repo_url} ${local.remote_terraform_path}; fi",
       "sudo ln -fs /usr/local/bin/ansible-playbook /usr/bin/ansible-playbook",
-      "sudo cp ${local.remote_inputs_path} ${local.remote_terraform_path}"
-      # "export TF_LOG=${var.TF_LOG} && sudo -E terraform -chdir=${local.remote_terraform_path} init && sudo -E terraform -chdir=${local.remote_terraform_path} apply -parallelism=${var.TF_PARALLELISM} -auto-approve"
+      "sudo cp ${local.remote_inputs_path} ${local.remote_terraform_path}",
+      "export TF_LOG=${var.TF_LOG} && sudo -E terraform -chdir=${local.remote_terraform_path} init"
+      # "sudo -E terraform -chdir=${local.remote_terraform_path} apply -parallelism=${var.TF_PARALLELISM} -auto-approve"
+      #"export TF_LOG=${var.TF_LOG} && sudo -E terraform -chdir=${local.remote_terraform_path} init && sudo -E terraform -chdir=${local.remote_terraform_path} apply -parallelism=${var.TF_PARALLELISM} -auto-approve"
     ]
   }
 
