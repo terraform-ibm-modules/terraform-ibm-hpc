85cd8e764999ba73c8c63ff0df443b5f288fd6ff
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-04-23 22:38:33 +0530

    Modified sg attachment logic
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- modules/landing_zone_vsi/locals.tf
+++ modules/landing_zone_vsi/locals.tf
@@ -285,13 +285,14 @@ locals {
   compute_public_key_content  = one(module.compute_key[*].public_key_content)
   compute_private_key_content = one(module.compute_key[*].private_key_content)
 
-  # Security Groups
-  protocol_secondary_security_group = [for subnet in local.protocol_subnets :
-    {
-      security_group_id = one(module.compute_sg[*].security_group_id)
-      interface_name    = subnet["name"]
-    }
-  ]
+  protocol_secondary_security_group = flatten([
+    for subnet_index, subnet in local.protocol_subnets : [
+      for i in range(var.protocol_instances[subnet_index]["count"]) : {
+        security_group_id = one(module.storage_sg[*].security_group_id)
+        interface_name    = "${subnet["name"]}-${i}"
+      }
+    ]
+  ])
 
   # ldap_instance_image_id = var.enable_ldap == true && var.ldap_server == "null" ? data.ibm_is_image.ldap_vsi_image[0].id : "null"
 }
