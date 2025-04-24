dc38aabb933b2548700370087f2661addb85bb3f
Sagar BK <Sagar.BK1@ibm.com>
2025-04-24 18:14:16 +0530

    created a new branch from mani for testing

--- modules/resource_provisioner/locals.tf
+++ modules/resource_provisioner/locals.tf
@@ -4,5 +4,5 @@ locals {
   deployer_path          = "/opt/ibm"
   remote_terraform_path  = format("%s/terraform-ibm-hpc", local.deployer_path)
   da_hpc_repo_url        = "https://github.com/terraform-ibm-modules/terraform-ibm-hpc.git"
-  da_hpc_repo_tag        = "mani-ldap" ###### change it to main in future
+  da_hpc_repo_tag        = "sagar-mani-reviewbranch" ###### change it to main in future
 }
