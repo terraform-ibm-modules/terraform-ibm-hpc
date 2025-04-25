409164e09a01643cb7ba8c1d72a913a7f3543dec
Louies-Jhony <louies.jhony@ibm.com>
2025-04-25 07:06:54 +0530

    Baremetal - Long Term DA

--- modules/baremetal/datasource.tf
+++ modules/baremetal/datasource.tf
@@ -6,4 +6,3 @@ data "ibm_is_image" "storage" {
   count = length(var.storage_servers)
   name  = var.storage_servers[count.index]["image"]
 }
-
--- modules/baremetal/locals.tf
+++ modules/baremetal/locals.tf
@@ -4,7 +4,7 @@ locals {
   storage_image_id  = data.ibm_is_image.storage[*].id
   storage_node_name = format("%s-%s", local.prefix, "strg")
   resource_group_id = data.ibm_resource_group.existing_resource_group.id
-  bms_interfaces = ["ens1", "ens2"]
+  bms_interfaces    = ["ens1", "ens2"]
   #storage_ssh_keys  = [for name in var.storage_ssh_keys : data.ibm_is_ssh_key.storage[name].id]
 
   # TODO: explore (DA always keep it true)
