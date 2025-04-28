d6437473f81ae4c8337ffeea8915e4ed8c3983a3
Louies-Jhony <louies.jhony@ibm.com>
2025-04-28 08:44:07 +0530

    BareMetal Server - DA Longterm

--- modules/baremetal/outputs.tf
+++ modules/baremetal/outputs.tf
@@ -1,4 +1,14 @@
 output "list" {
-  value       = [for m in module.storage_baremetal : m.baremetal_servers]
-  description = "List of Bare Metal Server IDs and Names"
+  description = "A list of VSI with name, id, zone, and primary ipv4 address"
+  value = flatten([
+    for module_instance in module.storage_baremetal : [
+      for server_key, server_details in module_instance.baremetal_servers :
+      {
+        id           = server_details.bms_server_id
+        name         = server_details.bms_server_name
+        ipv4_address = server_details.bms_server_ip
+        vni_id       = server_details.bms_vni_id
+      }
+    ]
+  ])
 }
