4b1f0046dbc2964c05cc147267c558a9a78597eb
Louies-Jhony <louies.jhony@ibm.com>
2025-05-07 15:25:23 +0530

    GKLM and KMS

--- modules/resource_provisioner/locals.tf
+++ modules/resource_provisioner/locals.tf
@@ -6,9 +6,9 @@ locals {
   da_hpc_repo_url             = "https://github.com/terraform-ibm-modules/terraform-ibm-hpc.git"
   da_hpc_repo_tag             = "JL_4850_develop" ###### change it to main in future
   remote_ansible_path         = format("%s/ibm-spectrumscale-cloud-deploy", local.deployer_path)
-  scale_cloud_infra_repo_url  = "https://github.com/jayeshh123/ibm-spectrum-scale-install-infra"
+  scale_cloud_infra_repo_url  = "https://github.com/IBM/ibm-spectrum-scale-install-infra"
   scale_cloud_infra_repo_name = "ibm-spectrum-scale-install-infra"
-  scale_cloud_infra_repo_tag  = "jay_scale_da_api"
+  scale_cloud_infra_repo_tag  = "scale_hpc"
   products                    = var.scheduler == "Scale" ? "scale" : "lsf"
   ssh_key_file                = "${path.root}/../../solutions/${local.products}/bastion_id_rsa"
 }
