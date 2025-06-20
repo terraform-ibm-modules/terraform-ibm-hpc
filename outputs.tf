2d6ad2d3520d292af2b6299983f2b5e3b931f1ff
nupurg-ibm <115140593+nupurg-ibm@users.noreply.github.com>
2025-06-20 15:17:35 +0530

    fixing output for app center (#247)
    * fixing output
    
    * fixing branch name
    
    ---------
    
    Co-authored-by: Nupur Goyal <nupurgoyal@Nupurs-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -20,7 +20,7 @@ output "ssh_to_deployer" {
 
 output "ssh_to_management_node" {
   description = "SSH command to connect to the management node"
-  value       = var.scheduler == "LSF" && (var.enable_deployer == false) && length(local.mgmt_hosts_ips) > 0 ? "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J ubuntu@${var.bastion_fip} vpcuser@${local.mgmt_hosts_ips[0]}" : null
+  value       = var.scheduler == "LSF" && (var.enable_deployer == false) && length(local.mgmt_hosts_ips) > 0 ? "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J ubuntu@${var.bastion_fip} lsfadmin@${local.mgmt_hosts_ips[0]}" : null
 }
 
 output "cloud_monitoring_url" {
@@ -31,4 +31,14 @@ output "cloud_monitoring_url" {
 output "cloud_logs_url" {
   value       = (var.enable_deployer == false) && (var.observability_logs_enable_for_management || var.observability_logs_enable_for_compute) ? module.cloud_monitoring_instance_creation[0].cloud_logs_url : null
   description = "IBM Cloud Logs URL"
+}
+
+output "application_center_tunnel" {
+  description = "Available if IBM Spectrum LSF Application Center GUI is installed"
+  value       = var.enable_deployer ? "" : local.ssh_cmd
+}
+
+output "application_center_url" {
+  description = "Available if IBM Spectrum LSF Application Center GUI is installed"
+  value       = "https://localhost:8443"
 }
