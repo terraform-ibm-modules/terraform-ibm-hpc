37fab9cc2bae317fb4195f0b732822c9ed164a0b
Anand.Reddy7 <Anand.Reddy7@ibm.com>
2025-06-23 13:01:58 +0530

    Fixing Ansible repo issue

--- modules/resource_provisioner/locals.tf
+++ modules/resource_provisioner/locals.tf
@@ -4,7 +4,7 @@ locals {
   deployer_path               = "/opt/ibm"
   remote_terraform_path       = format("%s/terraform-ibm-hpc", local.deployer_path)
   da_hpc_repo_url             = "github.com/terraform-ibm-modules/terraform-ibm-hpc.git"
-  da_hpc_repo_tag             = "lsf-da-20-june" ###### change it to main in future
+  da_hpc_repo_tag             = "anand-lsf-da-20-june" ###### change it to main in future
   remote_ansible_path         = format("%s/ibm-spectrumscale-cloud-deploy", local.deployer_path)
   scale_cloud_infra_repo_url  = "https://github.com/jayeshh123/ibm-spectrum-scale-install-infra"
   scale_cloud_infra_repo_name = "ibm-spectrum-scale-install-infra"
