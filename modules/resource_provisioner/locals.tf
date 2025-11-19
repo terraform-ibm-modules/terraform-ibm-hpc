deec066f0570cc1a55f7ce2bd42b2160d52f89f5
Sagar BK <Sagar.BK1@ibm.com>
2025-11-19 22:52:10 +0530

    new changes on the image file

--- modules/resource_provisioner/locals.tf
+++ modules/resource_provisioner/locals.tf
@@ -5,7 +5,7 @@ locals {
   remote_terraform_path  = format("%s/terraform-ibm-hpc", local.deployer_path)
   #  da_hpc_repo_url        = "github.ibm.com/workload-eng-services/HPCaaS.git"
   da_hpc_repo_url             = "github.com/terraform-ibm-modules/terraform-ibm-hpc.git"
-  da_hpc_lsf_repo_tag         = "v3.1.0"
+  da_hpc_lsf_repo_tag         = "v3.2.0"
   da_hpc_scale_repo_tag       = "v3.1.0"
   da_hpc_repo_tag             = var.scheduler == "Scale" ? local.da_hpc_scale_repo_tag : local.da_hpc_lsf_repo_tag
   remote_ansible_path         = format("%s/ibm-spectrumscale-cloud-deploy", local.deployer_path)
