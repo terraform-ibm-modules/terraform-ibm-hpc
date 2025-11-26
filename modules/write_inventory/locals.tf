0d66b75bbebb77de5c3cdfc67bc02376bd9d04e4
Sagar BK <Sagar.BK1@ibm.com>
2025-11-26 16:11:12 +0530

    adding new plan_crn

--- modules/write_inventory/locals.tf
+++ modules/write_inventory/locals.tf
@@ -11,7 +11,7 @@ locals {
   new_compute_image_id       = local.compute_image_found_in_map ? local.image_region_map[var.dynamic_compute_instances[0]["image"]][local.region] : "Image not found with the given name"
   image_id                   = local.compute_image_found_in_map ? local.new_compute_image_id : data.ibm_is_image.dynamic_compute[0].id
   catalog_offering = {
-    version_crn = "crn:v1:bluemix:public:globalcatalog-collection:global:a/77efe1030c00b5c89cfd08648d3480bf:0d89ec0d-d39a-494d-ac5b-9d940d8cc65f:version:61e655c5-40b6-4b68-a6ab-e6c77a457fce/e08b9ca5-699c-4779-8369-1a0c1ed54b30"
+    version_crn = "crn:v1:bluemix:public:globalcatalog-collection:global::1082e7d2-5e2f-0a11-a3bc-f88a8e1931fc:version:61e655c5-40b6-4b68-a6ab-e6c77a457fce-global/e08b9ca5-699c-4779-8369-1a0c1ed54b30-global"
     plan_crn    = "crn:v1:bluemix:public:globalcatalog-collection:global:a/77efe1030c00b5c89cfd08648d3480bf:0d89ec0d-d39a-494d-ac5b-9d940d8cc65f:plan:sw.0d89ec0d-d39a-494d-ac5b-9d940d8cc65f.d114e7ab-4f7e-40c4-98cc-f0c000cbf3a7"
   }
 }
