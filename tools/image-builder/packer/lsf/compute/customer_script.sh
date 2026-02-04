7753bbe8138f4d2ef2a0c05fcb7a0785ec459053
Sagar BK <Sagar.BK1@ibm.com>
2026-02-04 15:56:24 +0530

    adding changes on the gabor image

--- tools/image-builder/packer/lsf/compute/customer_script.sh
+++ tools/image-builder/packer/lsf/compute/customer_script.sh
@@ -9,6 +9,24 @@
 if grep -q 'ID="rhel"' /etc/os-release || grep -q 'ID="rocky"' /etc/os-release; then
     echo "================= Running user customization script ================="
     # Add your commands here for Red Hat or Rocky Linux
+    # Steps from: https://wiki.crowncloud.net/?How_to_Install_Python_3_12_on_CentOS_Stream_8
+    dnf install -y xz-devel gcc openssl-devel bzip2-devel libffi-devel wget tar make docker
+    cd /tmp
+    wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz
+    tar -xf Python-3.12.0.tgz
+    cd Python-3.12.0
+    ./configure --enable-optimizations
+    make -j $(nproc)
+    make altinstall
+    pip3.12 install --upgrade pip
+    # pip3.12 install requests dotenv pyscf ffsim matplotlib qiskit_ibm_runtime
+    pip3.12 install requests dotenv pyscf ffsim numpy matplotlib qiskit[visualization] qiskit_ibm_runtime qiskit_addon_sqd
+    cd /tmp
+    rm -rf Python-3.12.0
+    # dnf install -y python36-devel python36-devel
+    # dnf module enable python312
+    # dnf install -y python3.12-*
+    # pip-3.12 install requests dotenv pyscf ffsim matplotlib qiskit_ibm_runtime
 else
     echo "================= Running user customization script ================="
     # Add your commands here for Ubuntu
--- tools/image-builder/templates/packer_user_data.tpl
+++ tools/image-builder/templates/packer_user_data.tpl
@@ -63,7 +63,7 @@ echo "======================Cloning HPC public repo====================="
 sudo yum install git -y
 mkdir /LSF
 cd /LSF
-git clone https://github.com/terraform-ibm-modules/terraform-ibm-hpc.git
+git clone https://github.com/terraform-ibm-modules/terraform-ibm-hpc.git -b sagar-gabor-image
 cd /LSF/image-builder/solutions/lsf
 
 echo "======================Installing terraform====================="
