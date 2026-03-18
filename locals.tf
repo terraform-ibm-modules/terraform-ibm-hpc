af9b25c1dc7795b379599bb70029103d0d6b781d
Nupur Goyal <nupurgoyal@Nupurs-MacBook-Pro.local>
2026-03-18 17:42:00 +0530

    fixing pre-commit

--- locals.tf
+++ locals.tf
@@ -631,7 +631,6 @@ locals {
 #########################
 locals {
   bastion_fip                 = module.deployer.bastion_fip
-  observability_playbook_path = var.enable_deployer ? "${path.root}/../../modules/ansible-roles/observability.yaml" : "${path.root}/modules/ansible-roles/observability.yaml"
   lsf_mgmt_playbooks_path     = var.enable_deployer ? "${path.root}/../../modules/ansible-roles/lsf_mgmt_config.yml" : "${path.root}/modules/ansible-roles/lsf_mgmt_config.yml"
   playbooks_path              = var.enable_deployer ? "${path.root}/../../modules/ansible-roles/" : "${path.root}/modules/ansible-roles"
 }
